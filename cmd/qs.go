d6af2cbeb79ec6b50b4b46ff70b35bd45a5aa9f4
x1a0xf <yyzw@live.com>
2024-03-23 10:30:48 +0800

    feat: xxx

--- cmd/qs.go
+++ cmd/qs.go
@@ -27,58 +27,20 @@ var qsCmd = &cobra.Command{
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		docs := qs.NewDocs(wf.CacheDir() + "/qs.yml")
-
-		switch len(args) {
-		case 0:
-			// default: display all name
-			for _, doc := range docs {
-				for _, xxx := range doc.Xxx {
-					v := xxx.Name
-					wf.NewItem(v).Title(v).
-						Valid(true).
-						Arg(addMarkdownListFormat(xxx.Qs)).
-						Autocomplete(v).
-						Subtitle(fmt.Sprintf("#%s", doc.Cate))
-
-					// wf.NewItem(v).Title(v).Valid(true).Arg("111").Autocomplete(v).Subtitle(fmt.Sprintf("#%s", doc.Cate))
-				}
+		// default: display all name
+		for _, doc := range docs {
+			for _, xxx := range doc.Xxx {
+				v := xxx.Name
+				wf.NewItem(v).Title(v).Valid(true).
+					Arg(addMarkdownListFormat(docs.GetQsByName(v))).
+					Autocomplete(v).Subtitle(fmt.Sprintf("#%s", doc.Cate))
 			}
-
-			wf.SendFeedback()
-
-			// case 1:
-			// 	// determine
-			// 	query := args[0]
-			// 	// var qss []string
-			// 	// if docs.IsHitName(query) {
-			// 	// 	qss = docs.GetQsByName(query)
-			// 	// } else {
-			// 	// 	qss = docs.SearchQs(query)
-			// 	// }
-			// 	//
-			// 	// for _, qs := range qss {
-			// 	// 	wf.NewItem(qs).Title(qs).Valid(true).Arg(qs)
-			// 	// }
-			//
-			// 	for _, qs := range docs.GetQsByName(query) {
-			// 		wf.NewItem(qs).Title(qs).Valid(true).Arg(qs)
-			// 	}
-			//
-			// 	// wf.Filter(query)
-			// 	wf.SendFeedback()
-
-			// case 2:
-			// 	// vv name <qs>
-			// 	name := args[0]
-			// 	query := args[1]
-			//
-			// 	qss := docs.GetQsByName(name)
-			// 	for _, qs := range qss {
-			// 		wf.NewItem(qs).Title(qs).Valid(true).Arg(qs)
-			// 	}
-			// 	wf.Filter(query)
-			// 	wf.SendFeedback()
 		}
+
+		if len(args) > 0 {
+			wf.Filter(args[0])
+		}
+		wf.SendFeedback()
 	},
 }
 
