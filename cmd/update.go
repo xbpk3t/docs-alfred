50d71c56d70dbff886f4f572549339eb95af6dd4
x1a0xf <yyzw@live.com>
2024-03-25 15:51:25 +0800

    feat: 通过cobra的flag增加拓展性，之后可以直接在alfred后台添加config即可使用

--- cmd/update.go
+++ cmd/update.go
@@ -1,6 +1,3 @@
-/*
-Copyright ©
-*/
 package cmd
 
 import (
@@ -17,25 +14,21 @@ var EnabledAutoUpdate = "false"
 const updateJobName = "checkForUpdate"
 
 func CheckForUpdate() {
-	if EnabledAutoUpdate == "true" {
-		if wf.UpdateCheckDue() && !wf.IsRunning(updateJobName) {
-			logrus.Println("Running update check in background...")
-			cmd := exec.Command(os.Args[0], "update")
-			if err := wf.RunInBackground(updateJobName, cmd); err != nil {
-				logrus.Printf("Error starting update check: %s", err)
-			}
+	if wf.UpdateCheckDue() && !wf.IsRunning(updateJobName) {
+		logrus.Println("Running update check in background...")
+		cmd := exec.Command(os.Args[0], "update")
+		if err := wf.RunInBackground(updateJobName, cmd); err != nil {
+			logrus.Printf("Error starting update check: %s", err)
 		}
+	}
 
-		if wf.UpdateAvailable() {
-			wf.Configure(aw.SuppressUIDs(true))
-			wf.NewItem("An update is available!").
-				Subtitle("⇥ or ↩ to install update").
-				Valid(false).
-				Autocomplete("workflow:update").
-				Icon(&aw.Icon{Value: "update-available.png"})
-		}
-	} else {
-		logrus.Info("Auto update disabled")
+	if wf.UpdateAvailable() {
+		wf.Configure(aw.SuppressUIDs(true))
+		wf.NewItem("An update is available!").
+			Subtitle("⇥ or ↩ to install update").
+			Valid(false).
+			Autocomplete("workflow:update").
+			Icon(&aw.Icon{Value: "update-available.png"})
 	}
 }
 
