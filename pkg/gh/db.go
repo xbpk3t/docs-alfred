82de35d05a18607a8cbbac4fbfd5e318a2f19556
x1a0xf <yyzw@live.com>
2024-04-01 23:37:00 +0800

    fix: gh

--- pkg/gh/db.go
+++ pkg/gh/db.go
@@ -108,19 +108,19 @@ func (rs *Repos) ListRepositories(localDB string) error {
 	return nil
 }
 
-func (rs Repos) RemoveDuplicates() Repos {
-	uniqueValues := make(map[string]bool)
-	// result := make([]Repository, 0)
-
-	for _, t := range rs {
-		if !uniqueValues[t.URL] {
-			uniqueValues[t.URL] = true
-			rs = append(rs, t)
-		}
-	}
-
-	return rs
-}
+// func (rs Repos) RemoveDuplicates() Repos {
+// 	uniqueValues := make(map[string]bool)
+// 	// result := make([]Repository, 0)
+//
+// 	for _, t := range rs {
+// 		if !uniqueValues[t.URL] {
+// 			uniqueValues[t.URL] = true
+// 			rs = append(rs, t)
+// 		}
+// 	}
+//
+// 	return rs
+// }
 
 // func addMarkdownListFormat(str []string) string {
 // 	var builder strings.Builder
