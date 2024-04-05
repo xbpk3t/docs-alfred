a95e49de81cf7399ae53b717e306a454f360e65e
x1a0xf <yyzw@live.com>
2024-04-05 10:51:37 +0800

    feat: Add qs pkg

--- cmd/sync.go
+++ cmd/sync.go
@@ -1,54 +1,58 @@
 package cmd
 
 import (
-	"io"
 	"log/slog"
-	"net/http"
-
-	"github.com/91go/docs-alfred/pkg/gh"
 
 	"github.com/spf13/cobra"
 )
 
 const SyncJob = "sync"
 
+// const expire = 12
+
 // syncCmd represents the sync command
 var syncCmd = &cobra.Command{
 	Use:   "sync",
 	Short: "A brief description of your command",
 	Run: func(cmd *cobra.Command, args []string) {
-		url := wf.Config.GetString("url") + cfgFile
-		if url != "" {
-			resp, err := http.Get(url)
-			if err != nil {
-				slog.Error("request error", slog.Any("err", err))
-				return
-			}
-			defer resp.Body.Close()
+		// url := wf.Config.GetString("url") + cfgFile
 
-			data, err := io.ReadAll(resp.Body)
-			if err != nil {
-				return
-			}
-			err = wf.Cache.Store(cfgFile, data)
-			if err != nil {
-				return
-			}
-		}
-
-		switch cfgFile {
-		case "gh.yml":
-			token := wf.Config.GetString("gh_token")
-			gh := gh.NewRepos()
-			if _, err := gh.UpdateRepositories(token, wf.CacheDir()+"/repo.db"); err != nil {
-				// wf.NewWarningItem("Sync Failed.", err.Error()).Valid(false).Title("Sync Failed.")
-				// wf.SendFeedback()
-				// slog.Error("Sync Failed.", slog.Any("err", err))
-				ErrorHandle(err)
-			}
-		default:
-
-		}
+		// wf.Cache.LoadOrStore(cfgFile, time.Duration(expire)*time.Minute, func() ([]byte, error) {
+		//
+		// 	if url != "" {
+		// 		resp, err := http.Get(url)
+		// 		if err != nil {
+		// 			slog.Error("request error", slog.Any("err", err))
+		// 			return
+		// 		}
+		// 		defer resp.Body.Close()
+		//
+		// 		data, err := io.ReadAll(resp.Body)
+		// 		if err != nil {
+		// 			return
+		// 		}
+		// 		err = wf.Cache.Store(cfgFile, data)
+		// 		if err != nil {
+		// 			return
+		// 		}
+		// 	}
+		//
+		// 	switch cfgFile {
+		// 	case "gh.yml":
+		// 		token := wf.Config.GetString("gh_token")
+		// 		gh := gh.NewRepos()
+		// 		if _, err := gh.UpdateRepositories(token, wf.CacheDir()+"/repo.db"); err != nil {
+		// 			// wf.NewWarningItem("Sync Failed.", err.Error()).Valid(false).Title("Sync Failed.")
+		// 			// wf.SendFeedback()
+		// 			// slog.Error("Sync Failed.", slog.Any("err", err))
+		// 			ErrorHandle(err)
+		// 		}
+		// 	default:
+		//
+		// 	}
+		//
+		// 	return nil, nil
+		// })
 
 		slog.Info("Sync Repos Successfully.")
 	},
--- go.mod
+++ go.mod
@@ -3,12 +3,12 @@ module github.com/91go/docs-alfred
 go 1.21.0
 
 require (
-	github.com/91go/yaml2md v0.0.0-20240301122941-a72632e57d35
 	github.com/deanishe/awgo v0.29.1
 	github.com/google/go-github/v56 v56.0.0
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/samber/lo v1.39.0
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.8.0
 	gopkg.in/yaml.v3 v3.0.1
@@ -20,7 +20,6 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magefile/mage v1.11.0 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/samber/lo v1.39.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	go.deanishe.net/env v0.5.1 // indirect
