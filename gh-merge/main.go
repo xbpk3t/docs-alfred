fc9d505547bec973b58b9991100979452461999c
x1a0xf <yyzw@live.com>
2024-07-27 08:38:13 +0800

    fix

---
+++ gh-merge/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/hxhac/docs-alfred/gh-merge/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- merge-gh/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/hxhac/docs-alfred/merge-gh/cmd"
-
-func main() {
-	cmd.Execute()
-}
