0735f9490ea94ddeb7dbdb851d5ece3dd106372e
XBPk3T <yyzw@live.com>
2024-10-19 16:23:24 +0800

    feat: rss2newsletter

--- feeds-merge/pkg/merge.go
+++ feeds-merge/pkg/merge.go
@@ -5,27 +5,38 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
-	"strconv"
-	"sync"
 	"time"
 
-	"github.com/avast/retry-go"
 	"gopkg.in/yaml.v3"
 
+	"github.com/avast/retry-go"
 	"github.com/gorilla/feeds"
 	"github.com/mmcdole/gofeed"
 )
 
 type Config struct {
-	Path       string
-	Author     string
-	FeedLink   string
-	Categories []Categories `yaml:"categories"`
-	Timeout    int
-	FeedLimit  int // 每个feed只获取最近n条item
+	Feed       FeedCfg       `yaml:"feed"`
+	Resend     ResendCfg     `yaml:"resend"`
+	Newsletter NewsletterCfg `yaml:"newsletter"`
+	Categories
 }
 
-type Categories struct {
+type FeedCfg struct {
+	Timeout   int `yaml:"timeout"`
+	MaxTries  int `yaml:"maxTries"`
+	FeedLimit int `yaml:"feedLimit"`
+}
+type ResendCfg struct {
+	Token string `yaml:"token"`
+}
+type NewsletterCfg struct {
+	Schedule            string `yaml:"schedule"`
+	IsHideAuthorInTitle bool   `yaml:"isHideAuthorInTitle"`
+}
+
+type Categories []Category
+
+type Category struct {
 	Type string `yaml:"type"`
 	URLs []Feed `yaml:"urls"`
 }
@@ -35,59 +46,65 @@ type Feed struct {
 	Des  string `yaml:"des"`
 	URL  string `yaml:"url"`
 	Name string `yaml:"name"`
+	Date string
 }
 
-var (
-	once sync.Once
-	Conf *Config
+const (
+	Daily  = "daily"
+	Weekly = "weekly"
 )
 
-func NewConfig() *Config {
-	once.Do(func() {
-		// ti := EnvStrToInt("INPUT_CLIENT_TIMEOUT", 30)
-		// feedLimit := EnvStrToInt("INPUT_FEED_LIMIT", 300)
-		path := ReadEnv("INPUT_FEEDS_PATH", ".github/workspace/feeds.yml")
-		fx, err := os.ReadFile(path)
-		if err != nil {
-			return
-		}
-		var cates []Categories
-		err = yaml.Unmarshal(fx, &cates)
-		if err != nil {
-			slog.Info("Unmarshal:", slog.Any("Error", err))
-			return
-		}
-		Conf = &Config{
-			Path:       path,
-			Timeout:    30, // 最多retry 6次，所以设置为30（第一次retry 5s，则第6次 = 5 * 2^5 = 160s，合计315s）
-			FeedLimit:  20,
-			Author:     ReadEnv("INPUT_AUTHOR_NAME", "github-actions"),
-			FeedLink:   ReadEnv("INPUT_FEED_LINK", ""),
-			Categories: cates,
-		}
-	})
-	return Conf
+// 时间范围映射
+var scheduleTimeRanges = map[string]time.Duration{
+	Daily:  24 * time.Hour,
+	Weekly: 7 * 24 * time.Hour,
 }
 
-func ReadEnv(envKey, def string) string {
-	if val := os.Getenv(envKey); val != "" {
-		return val
-	}
-	return def
-}
+// func NewConfig() *Config {
+// 	fx, err := os.ReadFile(feedFile)
+// 	if err != nil {
+// 		slog.Error("Read FeedCfg File Error, Check your path:", slog.String("FeedCfg File", feedFile))
+// 		return nil
+// 	}
+// 	var cates []Category
+// 	err = yaml.Unmarshal(fx, &cates)
+// 	if err != nil {
+// 		slog.Error("Unmarshal:", slog.Any("Error", err))
+// 		return nil
+// 	}
+// 	return &Config{
+// 		// Timeout:    30,
+// 		// FeedLimit:  20,
+// 		Categories: cates,
+// 	}
+// }
 
-func EnvStrToInt(envKey string, def int) int {
-	val := os.Getenv(envKey)
-	ti, err := strconv.Atoi(val)
+func NewConfig(cfgFile, feedFile string) *Config {
+	fx, err := os.ReadFile(cfgFile)
 	if err != nil {
-		// core.Infof("set Config [%s] error: %v", envKey, err)
-		return def
+		return nil
 	}
-	return ti
+
+	var cfg Config
+	if err := yaml.Unmarshal(fx, &cfg); err != nil {
+		return nil
+	}
+
+	ff, err := os.ReadFile(feedFile)
+	if err != nil {
+		return nil
+	}
+	var cates Categories
+	if err := yaml.Unmarshal(ff, &cates); err != nil {
+		return nil
+	}
+	cfg.Categories = cates
+
+	return &cfg
 }
 
 // // fetchURL 直接获取
-// func (e Config) fetchURL(url string, ch chan<- *gofeed.Feed) {
+// func (e Config) fetchURL(url string, ch chan<- *gofeed.FeedCfg) {
 // 	// core.Infof("Fetching URL: %v\n", url)
 // 	fp := gofeed.NewParser()
 // 	fp.Client = &http.Client{
@@ -107,7 +124,7 @@ func (e Config) FetchURLWithRetry(url string, ch chan<- *gofeed.Feed) {
 	// core.Infof("Fetching URL: %v\n", url)
 	fp := gofeed.NewParser()
 	fp.Client = &http.Client{
-		Timeout: time.Duration(e.Timeout) * time.Second,
+		Timeout: time.Duration(e.Feed.Timeout) * time.Second,
 	}
 
 	var attempts uint = 0
@@ -121,17 +138,17 @@ func (e Config) FetchURLWithRetry(url string, ch chan<- *gofeed.Feed) {
 			ch <- feed // 如果没有错误，发送 feed 并结束重试
 			return nil
 		},
-		retry.Attempts(uint(e.Timeout/5)),   // 设置最大尝试次数，例如超时的5倍
-		retry.Delay(time.Second*5),          // 设置初始延迟
-		retry.DelayType(retry.BackOffDelay), // 设置退避策略
-		retry.LastErrorOnly(true),           // 只返回最后一个错误
+		retry.Attempts(uint(e.Feed.MaxTries)), // 设置最大尝试次数，例如超时的5倍
+		retry.Delay(time.Second*5),            // 设置初始延迟
+		retry.DelayType(retry.BackOffDelay),   // 设置退避策略
+		retry.LastErrorOnly(true),             // 只返回最后一个错误
 		retry.OnRetry(func(n uint, err error) {
 			attempts = n
-			slog.Info("Retry Parse Feed:", slog.String("URL", url), slog.Int("Tries", int(attempts)))
+			slog.Info("Retry Parse FeedCfg:", slog.String("URL", url), slog.Int("Tries", int(attempts)))
 		}),
 	)
 	if err != nil {
-		slog.Info("Parse Feed Error:", slog.String("URL", url), slog.Any("Error", err))
+		slog.Info("Parse FeedCfg Error:", slog.String("URL", url), slog.Any("Error", err))
 		ch <- nil // 发送 nil 表示获取失败
 	}
 
@@ -139,12 +156,12 @@ func (e Config) FetchURLWithRetry(url string, ch chan<- *gofeed.Feed) {
 	// if err == nil {
 	// 	ch <- feed
 	// } else {
-	// 	core.Infof("Parse Feed URL [%s] Error: (%v)\n", url, err)
+	// 	core.Infof("Parse FeedCfg URL [%s] Error: (%v)\n", url, err)
 	// 	ch <- nil
 	// }
 }
 
-// 批量提取URL为gofeed.Feed
+// 批量提取URL为gofeed.FeedCfg
 func (e Config) FetchURLs(urls []string) []*gofeed.Feed {
 	allFeeds := make([]*gofeed.Feed, 0)
 	ch := make(chan *gofeed.Feed)
@@ -161,7 +178,7 @@ func (e Config) FetchURLs(urls []string) []*gofeed.Feed {
 }
 
 // TODO: there must be a shorter syntax for this
-// type byPublished []*gofeed.Feed
+// type byPublished []*gofeed.FeedCfg
 //
 // func (s byPublished) Len() int {
 // 	return len(s)
@@ -194,26 +211,30 @@ func (e Config) getAuthor(feed *gofeed.Feed) string {
 		return feed.Title
 	}
 	// slog.Info("Using Default Author for", feed.Link)
-	return e.Author
+	return ""
 }
 
+// MergeAllFeeds
+// feedTitle type
+// allFeeds x
 func (e Config) MergeAllFeeds(feedTitle string, allFeeds []*gofeed.Feed) (*feeds.Feed, error) {
 	if len(allFeeds) == 0 {
 		return nil, errors.New("no feeds Found")
 	}
 
 	feed := &feeds.Feed{
-		Title:       feedTitle,
-		Link:        &feeds.Link{Href: e.FeedLink},
+		Title: feedTitle,
+		// Link:        &feeds.Link{Href: e.FeedLink},
 		Description: "Merged feeds from " + feedTitle,
-		Author: &feeds.Author{
-			Name: e.Author,
-		},
+		// Author: &feeds.Author{
+		// 	Name: e.Author,
+		// },
 		Created: time.Now(),
 	}
 	// sort.Sort(sort.Reverse(byPublished(allFeeds)))
-	limitPerFeed := e.FeedLimit
+	limitPerFeed := e.Feed.FeedLimit
 	seen := make(map[string]bool)
+	created := GetToday()
 	for _, sourceFeed := range allFeeds {
 		for i, item := range sourceFeed.Items {
 			if i > limitPerFeed {
@@ -222,31 +243,30 @@ func (e Config) MergeAllFeeds(feedTitle string, allFeeds []*gofeed.Feed) (*feeds
 			if seen[item.Link] {
 				continue
 			}
-			// created := item.PublishedParsed
-			// if created == nil {
-			// 	created = item.UpdatedParsed
-			// }
 
-			created := GetToday()
 			if item.UpdatedParsed != nil {
 				created = *item.UpdatedParsed
-			}
-			if item.PublishedParsed != nil {
+			} else if item.PublishedParsed != nil {
 				created = *item.PublishedParsed
 			}
-			if item.UpdatedParsed == nil && item.PublishedParsed == nil {
-				created = time.Now()
+
+			timeRange, exists := scheduleTimeRanges[e.Newsletter.Schedule]
+			if !exists {
+				// 如果不存在对应的时间范围，可以选择跳过或者使用默认值
+				continue
 			}
 
-			feed.Items = append(feed.Items, &feeds.Item{
-				Title: item.Title,
-				Link:  &feeds.Link{Href: item.Link},
-				// Description: item.Description,
-				Author:  &feeds.Author{Name: e.getAuthor(sourceFeed)},
-				Created: created,
-				// Content:     item.Content,
-			})
-			seen[item.Link] = true
+			if created.After(GetToday().Add(-timeRange)) {
+				feed.Items = append(feed.Items, &feeds.Item{
+					Title: item.Title,
+					Link:  &feeds.Link{Href: item.Link},
+					// Description: item.Description,
+					Author:  &feeds.Author{Name: e.getAuthor(sourceFeed)},
+					Created: created,
+					// Content:     item.Content,
+				})
+				seen[item.Link] = true
+			}
 		}
 	}
 	return feed, nil
@@ -256,4 +276,5 @@ func GetToday() time.Time {
 	timeStr := time.Now().Format("2006-01-02")
 	t, _ := time.ParseInLocation("2006-01-02 15:04:05", timeStr+" 00:00:00", time.Local)
 	return t
+	// return time.Now().Round(24 * time.Hour).Truncate(24 * time.Hour)
 }
