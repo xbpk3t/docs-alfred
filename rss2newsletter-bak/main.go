2c14594b24788b9ab26cf4b9b56c238c23d5a013
XBPk3T <yyzw@live.com>
2024-10-21 09:14:35 +0800

    feat: rename rss2newsletter with rss2newsletter-bak

--- rss2newsletter-bak/main.go
+++ rss2newsletter-bak/main.go
@@ -3,7 +3,7 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/xbpk3t/docs-alfred/rss2newsletter/cmd"
+import "github.com/xbpk3t/docs-alfred/rss2newsletter-bak/cmd"
 
 func main() {
 	cmd.Execute()
