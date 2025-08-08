9b8ad57c642425c94963d201462644c028a6806f
XBPk3T <yyzw@live.com>
2025-08-08 15:55:03 +0800

    fix: fix goods table sequence

--- service/goods/goods.go
+++ service/goods/goods.go
@@ -1,30 +1,31 @@
 package goods
 
 import (
+	"github.com/goccy/go-yaml"
 	"github.com/xbpk3t/docs-alfred/pkg/parser"
 	"github.com/xbpk3t/docs-alfred/service/gh"
 )
 
 // Goods 定义商品配置结构
 type Goods struct {
-	Tag    string     `yaml:"tag"`
-	Type   string     `yaml:"type"`
-	Des    string     `yaml:"des,omitempty"`
-	Topics []gh.Topic `yaml:"tpcs,omitempty"`
-	Item   []Item     `yaml:"item"`
-	Using  Item       `yaml:"using"`
-	Score  int        `yaml:"score"`
+	Tag    string          `yaml:"tag" json:"tag"`
+	Type   string          `yaml:"type" json:"type"`
+	Des    string          `yaml:"des,omitempty" json:"des"`
+	Using  Item            `yaml:"using" json:"using"`
+	Topics []gh.Topic      `yaml:"topics,omitempty" json:"topics"`
+	Item   []yaml.MapSlice `yaml:"item" json:"item"`
+	Record []string        `yaml:"record,omitempty" json:"record"`
+	Score  int             `yaml:"score" json:"score"`
 }
 
 // Item 定义单个商品项
 type Item struct {
-	Name   string   `yaml:"name"`
-	Param  string   `yaml:"param,omitempty"`
-	Price  string   `yaml:"price,omitempty"`
-	Des    string   `yaml:"des,omitempty"`
-	URL    string   `yaml:"url,omitempty"`
-	Record []string `yaml:"record,omitempty"`
-	Use    bool     `yaml:"use,omitempty"`
+	Name  string `yaml:"name" json:"name"`
+	Param string `yaml:"param,omitempty" json:"param"`
+	Price string `yaml:"price,omitempty" json:"price"`
+	Des   string `yaml:"des,omitempty" json:"des"`
+	URL   string `yaml:"url,omitempty" json:"url"`
+	Use   bool   `yaml:"use,omitempty" json:"use"`
 }
 
 // ParseConfig 解析配置文件
