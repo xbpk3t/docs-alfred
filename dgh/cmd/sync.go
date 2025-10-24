b553269a9ea30897c4af0cfa9893af2099e30caf
xbpk3t <yyzw@live.com>
2025-10-24 13:55:01 +0800

    fix(version): fix go.mod version

--- dgh/cmd/sync.go
+++ dgh/cmd/sync.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"github.com/xbpk3t/docs-alfred/dgh/pkg"
+	gh "github.com/xbpk3t/docs-alfred/dgh/pkg"
 )
 
 // syncCmd represents the sync command
